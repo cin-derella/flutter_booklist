466c31145af14679f6bdabeb296671f7949dbe82
cin-derella <shirley_cinderella@outlook.com>
2020-01-19 17:12:37 +0800

    add basic data for app

---
+++ lib/dummy_data.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id:'c1',
+    title: 'Literature',
+    color: Colors.purple,
+  ),
+    Category(
+    id:'c2',
+    title: 'Technology',
+    color: Colors.blue,
+  ),
+    Category(
+    id:'c3',
+    title: 'Sci-fi',
+    color: Colors.green,
+  ),
+    Category(
+    id:'c4',
+    title: 'Arts',
+    color: Colors.red,
+  ),
+    Category(
+    id:'c5',
+    title: 'Languages',
+    color: Colors.amber,
+  ),
+];
