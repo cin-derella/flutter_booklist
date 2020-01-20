0d908c25bcd13b02616d44f1490c03ec65af891a
cin-derella <shirley_cinderella@outlook.com>
2020-01-20 09:54:24 +0800

    style and theme

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('BookList'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -10,7 +10,7 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(title),
+      child: Text(title,style: Theme.of(context).textTheme.title,),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
@@ -20,7 +20,7 @@ class CategoryItem extends StatelessWidget {
           begin: Alignment.topLeft,
           end:Alignment.bottomRight,
         ),
-        borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(8),
       ),
     );
   }
