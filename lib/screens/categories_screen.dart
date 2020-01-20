66861cace0a4c50d8adee1c94671353694e19b54
cin-derella <shirley_cinderella@outlook.com>
2020-01-20 21:18:17 +0800

    add a tabbar to the appbar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,11 +5,7 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MealList'),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
@@ -27,7 +23,6 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-      ),
-    );
+      );
   }
 }
