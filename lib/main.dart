94be72c6894241ff3b7143b09d72e03192c685ca
cin-derella <shirley_cinderella@outlook.com>
2020-01-21 16:41:31 +0800

    add a favorite mark

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Map<String,bool> _filters = {
-    'gluten':false,
-    'lactose':false,
-    'vegan':false,
-    'vegetarian':false,
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
   };
 
   List<Meal> _availableMeals = DUMMY_MEALS;
 
-  void _setFilters(Map<String,bool> filterData){
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
     setState(() {
       _filters = filterData;
-      _availableMeals = DUMMY_MEALS.where((meal){
-        if(_filters['gluten']&& !meal.isGlutenFree){
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
           return false;
         }
-        if(_filters['lactose']&& !meal.isLactoseFree){
+        if (_filters['lactose'] && !meal.isLactoseFree) {
           return false;
         }
-        if(_filters['vegan']&& !meal.isVegan){
+        if (_filters['vegan'] && !meal.isVegan) {
           return false;
         }
-        if(_filters['vegetarian']&& !meal.isVegetarian){
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
           return false;
         }
         return true;
       }).toList();
     });
+  }
 
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+  bool _isMealFavorite(String id){
+    return _favoriteMeals.any((meal)=>meal.id== id);
   }
 
   @override
@@ -55,31 +76,32 @@ class _MyAppState extends State<MyApp> {
           accentColor: Colors.amber,
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           textTheme: ThemeData.light().textTheme.copyWith(
-                body1: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                body2: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                title: TextStyle(fontSize: 20,)
-              )),
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+              ))),
       //home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/' : (ctx)=>TabsScreen(),
-        CategoryMealsScreen.routeName:(ctx)=>CategoryMealsScreen(_availableMeals),
-        MealDetailScreen.routeName:(ctx)=>MealDetailScreen(),
-        FiltersScreen.routeName:(ctx)=>FiltersScreen(_filters,_setFilters),
+        '/': (ctx) => TabsScreen(_favoriteMeals),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeals),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite,_isMealFavorite),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
 
-      onGenerateRoute: (settings){
+      onGenerateRoute: (settings) {
         print(settings.arguments);
         //return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
       },
-      
-      onUnknownRoute: (settings){
-        return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
 
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
